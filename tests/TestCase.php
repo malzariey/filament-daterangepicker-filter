0f539a97e3258c9d994f160e58a1ac37343d3ce2
Majid Al Zariey <malzariey@gmail.com>
2023-03-08 17:54:07 +0300

    Adding Indicator

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace Malzariey\FilamentDaterangepickerFilter\Tests;
 
 use Filament\FilamentServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Malzariey\FilamentDaterangepickerFilter\FilamentDaterangepickerFilterServiceProvider;
@@ -14,9 +13,6 @@ class TestCase extends Orchestra
     {
         parent::setUp();
 
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Malzariey\\FilamentDaterangepickerFilter\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
@@ -30,11 +26,6 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-daterangepicker-filter_table.php.stub';
-        $migration->up();
-        */
     }
 }
