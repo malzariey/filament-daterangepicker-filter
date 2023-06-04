0634866c71610343ad2c50eff0042615e61620fc
Majid Al Zariey <malzariey@gmail.com>
2023-06-04 13:16:52 +0300

    Fix hover color, change default color to filament default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,29 @@
 const colors = require('tailwindcss/colors')
-
+function withOpacityValue(variable) {
+    return ({ opacityValue }) => {
+        if (opacityValue === undefined) {
+            return `rgb(var(${variable}))`
+        }
+        return `rgb(var(${variable}) / ${opacityValue})`
+    }
+}
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./resources/views/**/*.blade.php', './src/**/*.php'],
     darkMode: 'class',
+
     theme: {
         extend: {
             colors: {
                 danger: colors.rose,
-                primary: {
-                    "50": "#44dfeb",
-                    "100": "#3ad5e1",
-                    "200": "#30cbd7",
-                    "300": "#26c1cd",
-                    "400": "#1cb7c3",
-                    "500": "#12adb9",
-                    "600": "#08a3af",
-                    "700": "#0099a5",
-                    "800": "#008f9b",
-                    "900": "#008591"
-                },
+                primary: colors.amber,
                 success: colors.green,
                 warning: colors.amber,
             },
         },
     },
     corePlugins: {
-        preflight: true,
+        preflight: false,
     },
     plugins: [],
 }
