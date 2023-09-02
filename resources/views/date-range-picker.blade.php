cd72fe195861d0b3d7b505048493cdc5ff3d4599
Noxo <142592979+noxoua@users.noreply.github.com>
2023-09-02 07:48:46 +0000

    Improvements

--- resources/views/date-range-picker.blade.php
+++ resources/views/date-range-picker.blade.php
@@ -1,18 +1,25 @@
 @php
     $name = uniqid();
+    
+    $isPrefixInline = $isPrefixInline();
+    $isSuffixInline = $isSuffixInline();
+    $prefixActions = $getPrefixActions();
+    $prefixIcon = $getPrefixIcon();
+    $prefixLabel = $getPrefixLabel();
+    $suffixActions = $getSuffixActions();
+    $suffixIcon = $getSuffixIcon();
+    $suffixLabel = $getSuffixLabel();
 @endphp
+
 <x-dynamic-component
     :component="$getFieldWrapperView()"
     :field="$field"
 >
-    <div
-        wire:ignore
-    >
-    <div class="bg-[#ebf4f8]"></div>
-    <div
-        x-ref="container"
-        x-data="dateRangeComponent({
-                state:  @entangle($getStatePath()),
+    <div wire:ignore>
+        <div
+            x-ref="container"
+            x-data="dateRangeComponent({
+                state: @entangle($getStatePath()),
                 name: @js($name),
                 alwaysShowCalendars: @js($isAlwaysShowCalender()),
                 autoApply: @js($getAutoApplyOption()),
@@ -25,103 +32,101 @@
                 timePicker: @js($getTimePickerOption()),
                 timePickerIncrement: @js($getTimePickerIncrementOption()),
                 displayFormat: @js($getDisplayFormat()),
-                applyLabel: '{!!__('filament-daterangepicker-filter::message.apply')!!}',
-                cancelLabel: '{!!__('filament-daterangepicker-filter::message.cancel')!!}',
-                fromLabel: '{!!__('filament-daterangepicker-filter::message.from')!!}',
-                toLabel: '{!!__('filament-daterangepicker-filter::message.to')!!}',
-                customRangeLabel: '{!!__('filament-daterangepicker-filter::message.custom')!!}',
-                january :   '{!!__('filament-daterangepicker-filter::message.january')!!}',
-                february :  '{!!__('filament-daterangepicker-filter::message.february')!!}',
-                march:      '{!!__('filament-daterangepicker-filter::message.march')!!}',
-                april:      '{!!__('filament-daterangepicker-filter::message.april')!!}',
-                may:        '{!!__('filament-daterangepicker-filter::message.may')!!}',
-                june:       '{!!__('filament-daterangepicker-filter::message.june')!!}',
-                july:       '{!!__('filament-daterangepicker-filter::message.july')!!}',
-                august:     '{!!__('filament-daterangepicker-filter::message.august')!!}',
-                september:  '{!!__('filament-daterangepicker-filter::message.september')!!}',
-                october:    '{!!__('filament-daterangepicker-filter::message.october')!!}',
-                november :  '{!!__('filament-daterangepicker-filter::message.november')!!}',
-                december:   '{!!__('filament-daterangepicker-filter::message.december')!!}',
-                sunday:     '{!!__('filament-daterangepicker-filter::message.su')!!}',
-                monday:     '{!!__('filament-daterangepicker-filter::message.mo')!!}',
-                tuesday:    '{!!__('filament-daterangepicker-filter::message.tu')!!}',
-                wednesday:  '{!!__('filament-daterangepicker-filter::message.we')!!}',
-                thursday:   '{!!__('filament-daterangepicker-filter::message.th')!!}',
-                friday:     '{!!__('filament-daterangepicker-filter::message.fr')!!}',
-                saturday:   '{!!__('filament-daterangepicker-filter::message.sa')!!}',
-                firstDay:   @js($getFirstDayOfWeek()),
-                today:      '{!!__('filament-daterangepicker-filter::message.today')!!}',
-                yesterday:  '{!!__('filament-daterangepicker-filter::message.yesterday')!!}',
-                last_7_days:'{!!__('filament-daterangepicker-filter::message.last_7_days')!!}',
-                last_30_days:'{!!__('filament-daterangepicker-filter::message.last_30_days')!!}',
-                this_month: '{!!__('filament-daterangepicker-filter::message.this_month')!!}',
-                last_month: '{!!__('filament-daterangepicker-filter::message.last_month')!!}',
-                this_year:  '{!!__('filament-daterangepicker-filter::message.this_year')!!}',
-                last_year:  '{!!__('filament-daterangepicker-filter::message.last_year')!!}',
-                handleValueChangeUsing: ( value , name ) => {
-                    if(name == '{{$name}}'){
-                        @this.set('{!!$getStatePath()!!}',value);
+                applyLabel: '{!! __('filament-daterangepicker-filter::message.apply') !!}',
+                cancelLabel: '{!! __('filament-daterangepicker-filter::message.cancel') !!}',
+                fromLabel: '{!! __('filament-daterangepicker-filter::message.from') !!}',
+                toLabel: '{!! __('filament-daterangepicker-filter::message.to') !!}',
+                customRangeLabel: '{!! __('filament-daterangepicker-filter::message.custom') !!}',
+                january: '{!! __('filament-daterangepicker-filter::message.january') !!}',
+                february: '{!! __('filament-daterangepicker-filter::message.february') !!}',
+                march: '{!! __('filament-daterangepicker-filter::message.march') !!}',
+                april: '{!! __('filament-daterangepicker-filter::message.april') !!}',
+                may: '{!! __('filament-daterangepicker-filter::message.may') !!}',
+                june: '{!! __('filament-daterangepicker-filter::message.june') !!}',
+                july: '{!! __('filament-daterangepicker-filter::message.july') !!}',
+                august: '{!! __('filament-daterangepicker-filter::message.august') !!}',
+                september: '{!! __('filament-daterangepicker-filter::message.september') !!}',
+                october: '{!! __('filament-daterangepicker-filter::message.october') !!}',
+                november: '{!! __('filament-daterangepicker-filter::message.november') !!}',
+                december: '{!! __('filament-daterangepicker-filter::message.december') !!}',
+                sunday: '{!! __('filament-daterangepicker-filter::message.su') !!}',
+                monday: '{!! __('filament-daterangepicker-filter::message.mo') !!}',
+                tuesday: '{!! __('filament-daterangepicker-filter::message.tu') !!}',
+                wednesday: '{!! __('filament-daterangepicker-filter::message.we') !!}',
+                thursday: '{!! __('filament-daterangepicker-filter::message.th') !!}',
+                friday: '{!! __('filament-daterangepicker-filter::message.fr') !!}',
+                saturday: '{!! __('filament-daterangepicker-filter::message.sa') !!}',
+                firstDay: @js($getFirstDayOfWeek()),
+                today: '{!! __('filament-daterangepicker-filter::message.today') !!}',
+                yesterday: '{!! __('filament-daterangepicker-filter::message.yesterday') !!}',
+                last_7_days: '{!! __('filament-daterangepicker-filter::message.last_7_days') !!}',
+                last_30_days: '{!! __('filament-daterangepicker-filter::message.last_30_days') !!}',
+                this_month: '{!! __('filament-daterangepicker-filter::message.this_month') !!}',
+                last_month: '{!! __('filament-daterangepicker-filter::message.last_month') !!}',
+                this_year: '{!! __('filament-daterangepicker-filter::message.this_year') !!}',
+                last_year: '{!! __('filament-daterangepicker-filter::message.last_year') !!}',
+                handleValueChangeUsing: (value, name) => {
+                    if (name == '{{ $name }}') {
+                        @this.set('{!! $getStatePath() !!}', value);
                     }
                 },
             })"
-
-        id="date-range-picker-{{$name}}"
-        wire:key="date-range-picker-{{$name}}"
-        x-on:keydown.esc="isOpen() && $event.stopPropagation()"
-        {{ $attributes->merge($getExtraAttributes())->class(['filament-forms-date-time-picker-component relative']) }}
-        {{ $getExtraAlpineAttributeBag() }}
-    >
-        <div class="flex flex-row">
-            <button
-                x-ref="button"
-                aria-label="{{ $getPlaceholder() }}"
-                dusk="filament.forms.{{ $getStatePath() }}.open"
-                type="button"
-                tabindex="-1"
-                @if ($isDisabled()) disabled @endif
-                {{ $getExtraTriggerAttributeBag()->class([
-                    'bg-white dark:bg-white/5 relative w-full border py-2 pl-3 rtl:pr-3 text-start cursor-default rounded-lg shadow-sm outline-none',
-                    'focus-within:ring-1 focus-within:border-primary-500 focus-within:ring-inset focus-within:ring-primary-500' => ! $isDisabled(),
-                    'dark:bg-gray-700' => config('forms.dark_mode'),
-                    'border-gray-300' => ! $errors->has($getStatePath()),
-                    'dark:border-gray-600' => (! $errors->has($getStatePath())) && config('forms.dark_mode'),
-                    'border-danger-600' => $errors->has($getStatePath()),
-                    'dark:border-danger-400' => $errors->has($getStatePath()) && config('forms.dark_mode'),
-                    'opacity-70' => $isDisabled(),
-                    'dark:text-gray-300' => $isDisabled() && config('forms.dark_mode'),
-                ]) }}
+            id="date-range-picker-{{ $name }}"
+            wire:key="date-range-picker-{{ $name }}"
+            x-on:keydown.esc="isOpen() && $event.stopPropagation()"
+            {{ $attributes->merge($getExtraAttributes())->class(['filament-forms-date-time-picker-component relative']) }}
+            {{ $getExtraAlpineAttributeBag() }}
+        >
+            <x-filament::input.wrapper
+                :disabled="$isDisabled"
+                :inline-prefix="$isPrefixInline"
+                :inline-suffix="$isSuffixInline"
+                :prefix="$prefixLabel"
+                :prefix-actions="$prefixActions"
+                :prefix-icon="$prefixIcon"
+                :suffix="$suffixLabel"
+                :suffix-actions="$suffixActions"
+                :suffix-icon="$suffixIcon"
+                :valid="!$errors->has($statePath)"
+                class="fi-fo-text-input"
+                :attributes="\Filament\Support\prepare_inherited_attributes($getExtraAttributeBag())->class([
+                    'overflow-hidden',
+                ])"
             >
-                <div class="relative inline-block w-full px-2"  id="{{$name}}.container" wire:key="{{$name}}.container">
-                    <input
-                        readonly
-                        x-ref="daterange"
-                        name="{{$name}}"
-                        placeholder="{{$getPlaceholder()}}"
-                        wire:key="{{ $this->id() }}.{{ $getStatePath() }}.{{ $field::class }}.display-text"
-                        {!! ($id = $getId()) ? "id=\"{$id}\"" : null !!}
-                        @class([
-                            'w-full h-full p-0 placeholder-gray-400 bg-transparent border-0 outline-none focus:placeholder-gray-500 focus:ring-0 bg-transparent',
-                            'dark:bg-gray-700 dark:placeholder-gray-400' => config('forms.dark_mode'),
-                            'cursor-default' => $isDisabled(),
-                        ])
-                    />
-
-                    <span
-                        class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none rtl:right-auto rtl:left-0 rtl:pl-2">
-                        <svg @class([
-                            'w-5 h-5 text-gray-400',
-                            'dark:text-gray-400' => config('forms.dark_mode'),
-                        ]) xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
-                        </svg>
-                    </span>
-
-                </div>
-            </button>
-
+                <button
+                    x-ref="button"
+                    aria-label="{{ $getPlaceholder() }}"
+                    dusk="filament.forms.{{ $getStatePath() }}.open"
+                    type="button"
+                    tabindex="-1"
+                    class="w-full"
+                >
+                    <div
+                        class="relative inline-block w-full bg-white dark:bg-white/5"
+                        id="{{ $name }}.container"
+                        wire:key="{{ $name }}.container"
+                    >
+                        <x-filament::input
+                            x-ref="daterange"
+                            wire:key="{{ $this->id() }}.{{ $getStatePath() }}.{{ $field::class }}.display-text"
+                            :attributes="\Filament\Support\prepare_inherited_attributes(
+                                $getExtraInputAttributeBag(),
+                            )->merge(
+                                [
+                                    'id' => $getId(),
+                                    'name' => $name,
+                                    'disabled' => $isDisabled(),
+                                    'placeholder' => $getPlaceholder(),
+                                    'required' => $isRequired(),
+                                    'readonly' => true,
+                                    'type' => 'text',
+                                ],
+                                escape: false,
+                            )"
+                        />
+                    </div>
+                </button>
+            </x-filament::input.wrapper>
         </div>
-    </div>
-
     </div>
 </x-dynamic-component>
--- src/Fields/DateRangePicker.php
+++ src/Fields/DateRangePicker.php
@@ -4,14 +4,17 @@ namespace Malzariey\FilamentDaterangepickerFilter\Fields;
 
 use Carbon\CarbonInterface;
 use Closure;
-use Filament\Forms;
-use Filament\Forms\Components\Concerns;
+use Filament\Forms\Components\Concerns\{HasAffixes, HasExtraInputAttributes, HasPlaceholder};
+use Filament\Forms\Components\Contracts\HasAffixActions;
+use Filament\Forms\Components\Field;
 use Filament\Support\Concerns\HasExtraAlpineAttributes;
 use Illuminate\View\ComponentAttributeBag;
 
-class DateRangePicker extends Forms\Components\Field
+class DateRangePicker extends Field implements HasAffixActions
 {
-    use Concerns\HasPlaceholder;
+    use HasPlaceholder;
+    use HasAffixes;
+    use HasExtraInputAttributes;
     use HasExtraAlpineAttributes;
 
     protected string $view = 'filament-daterangepicker-filter::date-range-picker';
@@ -32,15 +35,24 @@ class DateRangePicker extends Forms\Components\Field
     protected string|Closure|null $timezone = null;
     protected array|Closure $disabledDates = [];
 
+    public static function make(string $name) : static
+    {
+        $static = parent::make($name);
+
+        $static->suffixIcon('heroicon-m-calendar-days');
+
+        return $static;
+    }
+
     //Javascript Format
-    public function displayFormat(string|Closure|null $format): static
+    public function displayFormat(string|Closure|null $format) : static
     {
         $this->displayFormat = $format;
 
         return $this;
     }
 
-    public function firstDayOfWeek(int|null $day): static
+    public function firstDayOfWeek(int|null $day) : static
     {
         if ($day < 0 || $day > 7) {
             $day = $this->getDefaultFirstDayOfWeek();
@@ -51,35 +63,35 @@ class DateRangePicker extends Forms\Components\Field
         return $this;
     }
 
-    public function format(string|Closure|null $format): static
+    public function format(string|Closure|null $format) : static
     {
         $this->format = $format;
 
         return $this;
     }
 
-    public function maxDate(CarbonInterface|string|Closure|null $date): static
+    public function maxDate(CarbonInterface|string|Closure|null $date) : static
     {
         $this->maxDate = $date;
 
         return $this;
     }
 
-    public function minDate(CarbonInterface|string|Closure|null $date): static
+    public function minDate(CarbonInterface|string|Closure|null $date) : static
     {
         $this->minDate = $date;
 
         return $this;
     }
 
-    public function startDate(CarbonInterface|string|Closure|null $date): static
+    public function startDate(CarbonInterface|string|Closure|null $date) : static
     {
         $this->startDate = $date;
 
         return $this;
     }
 
-    public function endDate(CarbonInterface|string|Closure|null $date): static
+    public function endDate(CarbonInterface|string|Closure|null $date) : static
     {
         $this->endDate = $date;
 
@@ -97,21 +109,21 @@ class DateRangePicker extends Forms\Components\Field
     }
 
 
-    public function disabledDates(array|Closure $dates): static
+    public function disabledDates(array|Closure $dates) : static
     {
         $this->disabledDates = $dates;
 
         return $this;
     }
 
-    public function resetFirstDayOfWeek(): static
+    public function resetFirstDayOfWeek() : static
     {
         $this->firstDayOfWeek($this->getDefaultFirstDayOfWeek());
 
         return $this;
     }
 
-    public function timezone(string|Closure|null $timezone): static
+    public function timezone(string|Closure|null $timezone) : static
     {
         $this->timezone = $timezone;
 
@@ -119,7 +131,7 @@ class DateRangePicker extends Forms\Components\Field
     }
 
 
-    public function getDisplayFormat(): string
+    public function getDisplayFormat() : string
     {
         $format = $this->evaluate($this->displayFormat);
 
@@ -127,11 +139,11 @@ class DateRangePicker extends Forms\Components\Field
             return $format;
         }
 
-        if (!$this->hasTime()) {
+        if (! $this->hasTime()) {
             return config('forms.components.date_time_picker.display_formats.date', 'M j, Y');
         }
 
-        if (!$this->hasDate()) {
+        if (! $this->hasDate()) {
             return $this->hasSeconds() ?
                 config('forms.components.date_time_picker.display_formats.time_with_seconds', 'H:i:s') :
                 config('forms.components.date_time_picker.display_formats.time', 'H:i');
@@ -142,7 +154,7 @@ class DateRangePicker extends Forms\Components\Field
             config('forms.components.date_time_picker.display_formats.date_time', 'M j, Y H:i');
     }
 
-    public function getExtraTriggerAttributes(): array
+    public function getExtraTriggerAttributes() : array
     {
         $temporaryAttributeBag = new ComponentAttributeBag();
 
@@ -153,17 +165,17 @@ class DateRangePicker extends Forms\Components\Field
         return $temporaryAttributeBag->getAttributes();
     }
 
-    public function getExtraTriggerAttributeBag(): ComponentAttributeBag
+    public function getExtraTriggerAttributeBag() : ComponentAttributeBag
     {
         return new ComponentAttributeBag($this->getExtraTriggerAttributes());
     }
 
-    public function getFirstDayOfWeek(): int
+    public function getFirstDayOfWeek() : int
     {
         return $this->firstDayOfWeek ?? $this->getDefaultFirstDayOfWeek();
     }
 
-    public function getFormat(): string
+    public function getFormat() : string
     {
         $format = $this->evaluate($this->format);
 
@@ -173,82 +185,82 @@ class DateRangePicker extends Forms\Components\Field
 
         $format = $this->hasDate() ? 'Y-m-d' : '';
 
-        if (!$this->hasTime()) {
+        if (! $this->hasTime()) {
             return $format;
         }
 
         $format = $format ? "{$format} H:i" : 'H:i';
 
-        if (!$this->hasSeconds()) {
+        if (! $this->hasSeconds()) {
             return $format;
         }
 
         return "{$format}:s";
     }
 
-    public function getMaxDate(): ?string
+    public function getMaxDate() : ?string
     {
         return $this->evaluate($this->maxDate);
     }
 
-    public function getMinDate(): ?string
+    public function getMinDate() : ?string
     {
         return $this->evaluate($this->minDate);
     }
 
-    public function getDisabledDates(): array
+    public function getDisabledDates() : array
     {
         return $this->evaluate($this->disabledDates);
     }
 
-    public function getTimezone(): string
+    public function getTimezone() : string
     {
         return $this->evaluate($this->timezone) ?? config('app.timezone');
     }
 
-    protected function getDefaultFirstDayOfWeek(): int
+    protected function getDefaultFirstDayOfWeek() : int
     {
         return config('forms.components.date_time_picker.first_day_of_week', 1);
     }
 
-    public function alwaysShowCalender(bool|Closure $condition = true): static
+    public function alwaysShowCalender(bool|Closure $condition = true) : static
     {
         $this->alwaysShowCalender = $condition;
 
         return $this;
     }
 
-    public function isAlwaysShowCalender(): bool
+    public function isAlwaysShowCalender() : bool
     {
         return $this->alwaysShowCalender;
     }
 
-    public function setTimePickerOption(bool $condition = true): static
+    public function setTimePickerOption(bool $condition = true) : static
     {
         $this->timePicker = $condition;
 
         return $this;
     }
 
-    public function getTimePickerOption(): string
+    public function getTimePickerOption() : string
     {
         return $this->timePicker ? 'true' : 'false';
     }
 
-    public function setTimePickerIncrementOption(int $increment = 1): static
+    public function setTimePickerIncrementOption(int $increment = 1) : static
     {
         $this->timePickerIncrement = $increment;
 
         return $this;
     }
 
-    public function getTimePickerIncrementOption(): int
+    public function getTimePickerIncrementOption() : int
     {
         return $this->timePickerIncrement;
     }
 
 
-    public function setAutoApplyOption(bool $condition = true): static
+    public function setAutoApplyOption(bool $condition = true) : static
     {
         $this->autoApply = $condition;
 
@@ -256,22 +268,20 @@ class DateRangePicker extends Forms\Components\Field
     }
 
     // NOTE: auto apply will not be enabled by daterangepicker.js if timePicker is set
-    public function getAutoApplyOption(): string
+    public function getAutoApplyOption() : string
     {
         return $this->autoApply ? 'true' : 'false';
     }
 
-    public function setLinkedCalendarsOption(bool $condition = true): static
+    public function setLinkedCalendarsOption(bool $condition = true) : static
     {
         $this->linkedCalendars = $condition;
 
         return $this;
     }
 
-    public function getLinkedCalendarsOption(): string
+    public function getLinkedCalendarsOption() : string
     {
         return $this->linkedCalendars ? 'true' : 'false';
     }
-
-
 }
