171a058b4b2c843377eea016534085791e809513
Noxo <142592979+noxoua@users.noreply.github.com>
2023-09-14 07:01:15 +0000

    made the ranges changeable

--- src/Fields/DateRangePicker.php
+++ src/Fields/DateRangePicker.php
@@ -34,12 +34,24 @@ class DateRangePicker extends Field implements HasAffixActions
     protected CarbonInterface|string|Closure|null $endDate = null;
     protected string|Closure|null $timezone = null;
     protected array|Closure $disabledDates = [];
+    protected array|Closure $ranges = [];
 
     public static function make(string $name) : static
     {
         $static = parent::make($name);
 
         $static->suffixIcon('heroicon-m-calendar-days');
+
+        $static->ranges([
+            __('filament-daterangepicker-filter::message.today') => [now(), now()],
+            __('filament-daterangepicker-filter::message.yesterday') => [now()->subDay(), now()->subDay()],
+            __('filament-daterangepicker-filter::message.last_7_days') => [now()->subDays(6), now()],
+            __('filament-daterangepicker-filter::message.last_30_days') => [now()->subDays(29), now()],
+            __('filament-daterangepicker-filter::message.this_month') => [now()->startOfMonth(), now()->endOfMonth()],
+            __('filament-daterangepicker-filter::message.last_month') => [now()->subMonth()->startOfMonth(), now()->subMonth()->endOfMonth()],
+            __('filament-daterangepicker-filter::message.this_year') => [now()->startOfYear(), now()->endOfYear()],
+            __('filament-daterangepicker-filter::message.last_year') => [now()->subYear()->startOfYear(), now()->subYear()->endOfYear()],
+        ]);
 
         return $static;
     }
@@ -130,6 +142,12 @@ class DateRangePicker extends Field implements HasAffixActions
         return $this;
     }
 
+    public function ranges(array|Closure $ranges) : static
+    {
+        $this->ranges = $ranges;
+
+        return $this;
+    }
 
     public function getDisplayFormat() : string
     {
@@ -283,5 +301,18 @@ class DateRangePicker extends Field implements HasAffixActions
     public function getLinkedCalendarsOption() : string
     {
         return $this->linkedCalendars ? 'true' : 'false';
+    }
+
+    public function getRanges() : ?array
+    {
+        $ranges = $this->evaluate($this->ranges);
+
+        foreach ($ranges as $key => $dates) {
+            $ranges[$key] = array_map(function ($date) {
+                return $date instanceof \Carbon\Carbon ? $date->toDateString() : $date;
+            }, $dates);
+        }
+
+        return $ranges;
     }
 }
