7473a852575c1c4e1182417339015711dd5e2201
Majid Al Zariey <malzariey@gmail.com>
2024-07-02 00:28:29 +0300

    Fix change initial value

--- resources/js/plugin.js
+++ resources/js/plugin.js
@@ -1163,7 +1163,7 @@
             }
 
             //if a new date range was selected, invoke the user callback function
-            if (!this.startDate.isSame(this.oldStartDate) || !this.endDate.isSame(this.oldEndDate))
+            // if (!this.startDate.isSame(this.oldStartDate) || !this.endDate.isSame(this.oldEndDate))
                 this.callback(this.startDate.clone(), this.endDate.clone(), this.chosenLabel);
 
             //if picker is attached to a text input, update it
