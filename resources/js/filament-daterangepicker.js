8ef94710c9f7979600e217987e7a168755cc1740
Julian Gums <julian@synt.ax>
2025-06-17 21:25:35 +0200

    Fix date duplication issue - final working solution Fixes moment.js object mutation and timezone boundary issues: - Clone moment objects before calling startOf('day') to prevent mutation - Convert disabled UTC dates to user timezone before comparison - Resolves issue #186 where dates showed as 1,2,2,4 instead of 1,2,3,4 - Tested with Europe/Jersey server + Germany user timezone scenario

--- resources/js/filament-daterangepicker.js
+++ resources/js/filament-daterangepicker.js
@@ -164,7 +164,7 @@ export default function dateRangeComponent({
                     isInvalidDate: (date) => {
                         if(momentDatesArray != null && momentDatesArray.length > 0 ) {
                             return momentDatesArray.some(disabledDate =>
-                                disabledDate.clone().startOf('day').isSame(date.clone().utc().startOf('day'), 'day')
+                                disabledDate.clone().tz(timezone).startOf('day').isSame(date.clone().startOf('day'), 'day')
                             );
                         }else{
                             return false;
